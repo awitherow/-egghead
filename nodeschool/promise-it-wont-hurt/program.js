a04630b2605b595c53dc174aa06d34369cbda1ce
awitherow <au.witherow@gmail.com>
2017-04-04 16:07:49 +0200

    feat(promise): disallow rejection after fulfillment example

--- nodeschool/promise-it-wont-hurt/program.js
+++ nodeschool/promise-it-wont-hurt/program.js
@@ -3,14 +3,15 @@ require('es6-promise');
 'use strict';
 
 var promise = new Promise(function (fulfill, reject) {
-  setTimeout(function() { 
-    reject(new Error('REJECTED!'));
-  }, 300);
-}).then(null, function(err) {
-  onReject(err);
+  fulfill('I FIRED');
+  reject(new Error('I DID NOT FIRE'));
+  
+}).then(function(success) {
+  console.log(success);
+}, function(err) {
+  onRejected(err);
 });
 
-function onReject(error) {
+function onRejected(error) {
   console.log(error.message);
 }
-

b34c5e3b7e45ce0191d3544a945f6304542c3813
awitherow <au.witherow@gmail.com>
2017-04-05 15:37:44 +0200

    chore(important): promises always reject on errors

--- nodeschool/promise-it-wont-hurt/program.js
+++ nodeschool/promise-it-wont-hurt/program.js
@@ -2,14 +2,21 @@ require('es6-promise');
 
 ('use strict');
 
-function parsePromised(json) {
-    return new Promise(function(fulfill, reject) {
-        try {
-            fulfill(JSON.parse(json));
-        } catch (e) {
-            reject(e);
-        }
-    });
+function alwaysThrows() {
+    throw new Error('OH NOES');
 }
 
-parsePromised(process.argv[2]).then(null, console.log);
+function iterate(int) {
+    console.log(int);
+    return int + 1;
+}
+
+var doThatShit = new Promise(function(f, r) {
+    for (let i = 1; i <= 10; i++) {
+        if (i > 5) {
+            alwaysThrows(); // auto rejects!
+        } else {
+            iterate(i);
+        }
+    }
+}).then(null, console.log);

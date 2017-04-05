f4c9ba9a91dd9e6414c4780435f6c493e8c3c4bf
awitherow <au.witherow@gmail.com>
2017-04-05 14:55:55 +0200

    chore(promise_after_promise): chaining promises example

--- nodeschool/promise-it-wont-hurt/program.js
+++ nodeschool/promise-it-wont-hurt/program.js
@@ -2,17 +2,4 @@ require('es6-promise');
 
 ('use strict');
 
-var promise = new Promise(function() {}).catch(function(err) {
-    console.error(err);
-});
-
-function SelectAOrB(aOrB) {
-    if (aOrB === 'a') {
-        Promise.resolve('yours is correct selection!');
-    } else {
-        Promise.reject(new Error('yours is not the correct selection!'));
-    }
-}
-
-SelectAOrB('a');
-SelectAOrB('b');
+first().then(second).then(console.log);
